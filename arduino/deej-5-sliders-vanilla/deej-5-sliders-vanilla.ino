72e929942eb8df3ffb386413c1b7802d41be6ca2
Random <aalfz@sina.com>
2021-07-28 18:36:54 +0800

    Add 16x2 LCD

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,13 +1,19 @@
 const int NUM_SLIDERS = 6;
 const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A6, A7};
-
 int analogSliderValues[NUM_SLIDERS];
+
+//add LCD library of 16x2 LCD shield;
+#include "rgb_lcd.h"
+rgb_lcd lcd;
 
 void setup() { 
   for (int i = 0; i < NUM_SLIDERS; i++) {
     pinMode(analogInputs[i], INPUT);
   }
 
+//initialise the lcd screen;
+  lcd.begin(16,2);
+  
   Serial.begin(9600);
 }
 
@@ -15,6 +21,16 @@ void loop() {
   updateSliderValues();
   sendSliderValues(); // Actually send data (all the time)
   // printSliderValues(); // For debug
+
+  lcd.setCursor(0,0);
+  lcd.print("1:");
+  lcd.setCursor(2,0);
+  lcd.print("Master");
+  lcd.setCursor(0,1);
+  lcd.print("2:");
+  lcd.setCursor(3,1);
+  lcd.print("Chrome");
+  
   delay(10);
 }
 
